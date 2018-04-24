e16727819d03a6d388d5a1f27bd68a5581fbeab6
rnc0011 <rnc0011@alu.ubu.es>
2018-04-24 16:35:27 +0200

    Se ha movido el método getAmount.

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -58,23 +58,10 @@ public class Customer {
 		return result;
 	}
 
+	/**
+	 * @deprecated Use {@link ubu.gii.dass.refactoring.Rental#getAmountFor()} instead
+	 */
 	private double getAmount(Rental aRental) {
-		double thisAmount = 0;
-		switch (aRental.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (aRental.getDaysRented() > 2)
-				thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			thisAmount += aRental.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (aRental.getDaysRented() > 3)
-				thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return thisAmount;
+		return aRental.getAmountFor();
 	}
 }
