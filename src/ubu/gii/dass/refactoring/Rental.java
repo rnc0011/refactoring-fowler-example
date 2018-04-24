2e57e17754a2591bd20ef75aad69ba4103326693
rnc0011 <rnc0011@alu.ubu.es>
2018-04-24 16:36:09 +0200

    Se ha añadido el método getAmountFor movido de la clase Customer.

--- src/ubu/gii/dass/refactoring/Rental.java
+++ src/ubu/gii/dass/refactoring/Rental.java
@@ -2,10 +2,10 @@ package ubu.gii.dass.refactoring;
 /**
  * Tema Refactorizaciones
  * 
- * Ejemplo de aplicación de refactorizaciones. Actualizado para colecciones
- * genéricas de java 1.5
+ * Ejemplo de aplicaciï¿½n de refactorizaciones. Actualizado para colecciones
+ * genï¿½ricas de java 1.5
  * 
- * @author M. Fowler y <A HREF="mailto:clopezno@ubu.es">Carlos López</A>
+ * @author M. Fowler y <A HREF="mailto:clopezno@ubu.es">Carlos Lï¿½pez</A>
  * @version 1.1
  * @see java.io.File
  * 
@@ -25,6 +25,26 @@ public class Rental {
 
 	public Movie getMovie() {
 		return _movie;
+	}
+
+	public double getAmountFor() {
+		double thisAmount = 0;
+		switch (getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			thisAmount += 2;
+			if (getDaysRented() > 2)
+				thisAmount += (getDaysRented() - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			thisAmount += getDaysRented() * 3;
+			break;
+		case Movie.CHILDRENS:
+			thisAmount += 1.5;
+			if (getDaysRented() > 3)
+				thisAmount += (getDaysRented() - 3) * 1.5;
+			break;
+		}
+		return thisAmount;
 	}
 
 }
